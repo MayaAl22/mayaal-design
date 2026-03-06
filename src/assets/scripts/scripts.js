31b39ee6df29c6fdcd99d865f1e1e97640391e9b
Markus Merklinger <m.merklinger@mailbox.org>
2026-03-06 11:34:06 +0100

    Rename directory for static files

--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -20,14 +20,14 @@ function hideMenu() {
   const header_nav = document.querySelector('header > nav');
   const menu_button = document.querySelector('#menu-button');
   header_nav.style.display = 'none';
-  menu_button.style.background = 'url("/static/images/menu-button-opening.svg")';
+  menu_button.style.background = 'url("/assets/images/menu-button-opening.svg")';
 }
 
 function showMenu() {
   const header_nav = document.querySelector('header > nav');
   const menu_button = document.querySelector('#menu-button');
   header_nav.style.display = 'block';
-  menu_button.style.background = 'url("/static/images/menu-button-closing.svg")';
+  menu_button.style.background = 'url("/assets/images/menu-button-closing.svg")';
 }
 
 function toggleMenu() {
